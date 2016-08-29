5a471be56b8c778c9b584c682a573af36035e692
Sergej Kasper <sergejkasper@gmail.com>
2016-08-30 01:30:28 +0200

    refactor book info fetching

--- src/main/webapp/app/entities/book/book-dialog.controller.js
+++ src/main/webapp/app/entities/book/book-dialog.controller.js
@@ -23,49 +23,38 @@
             vm.isSaving = true;
             $http.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + vm.book.bookIsbn).then(function(res){
                 if(!res.data.items){
-                    AlertService.error("Keine passenden Bücher gefunden.");
-                    return;
-                }
-                var resp = res.data.items[0];
-                if (resp){
-                    vm.book.title = resp.volumeInfo.title;
-                    vm.book.description = resp.volumeInfo.description.substring(0, 252);
-                    if(vm.book.description.length === 252) vm.book.description = vm.book.description + "...";
-                    switch(resp.volumeInfo.language){
-                        case "en":
-                        vm.book.language = "ENGLISCH";
-                        break;
-                        case "de":
-                        vm.book.language = "DEUTSCH";
-                        break;
-                        case "ru":
-                        vm.book.language = "RUSSISCH";
-                        break;
-                    }
-                    vm.book.cover = resp.volumeInfo.imageLinks.thumbnail;
-                    vm.book.coverContentType = "";
-                    var authorName = resp.volumeInfo.authors.join(", ");
-                    if(!vm.authors.some(function(author){
-                        if(author.name === authorName){
-                            vm.book.author = author;
-                            vm.isSaving = false;
-                            return true;
+                   /* $http.get('http://isbndb.com/api/v2/json/W25MX2G5/books?q=9781470196158&cacheBuster=1472481718355').then(function(res){
+                        if(!res.result_count || !res.result_count < 1){
+                            AlertService.error("Keine passenden Bücher gefunden.");
+                            return;
                         }
-                        return false;
-                    })){
-                        Author.save({'name': authorName}, function(result){
-                            vm.authors.push(result);
-                            vm.book.author = vm.authors[vm.authors.length - 1];
-
-                            vm.isSaving = false;
-                        }, function(){
-                            vm.isSaving = false;
-                        });
-                    }
+                        var resp = res.data[0];
+                        this.addBook(resp.title,
+                            resp.summary,
+                            resp.language,
+                            resp.author_data.map(function(a){
+                                // reformat: surename, name to name surname and return string[]
+                                var name = a.name.split(", ");
+                                return name[1] + " " + name[0];
+                            }));
+                    }, function(err){
+                        alert("Ein Fehler ist aufgetreten: ", JSON.stringify(err));
+                        return;
+                    })*/
+                }
+                return;
+                var resp = res.data.items[0].volumeInfo;
+                if (resp){
+                    this.addBook(resp.title,
+                        resp.description,
+                        resp.language,
+                        resp.imageLinks.thumbnail,
+                        resp.authors);
                 }
 
             }, function(err){
-                alert(err);
+                alert("Ein Fehler ist aufgetreten: ", JSON.stringify(err));
+                return;
             })
         };
 
@@ -77,6 +66,43 @@
         $timeout(function (){
             angular.element('.form-group:eq(1)>input').focus();
         });
+
+        function addBook(title, description, language, cover, authors){
+            vm.book.title = title;
+            vm.book.description = description.substring(0, 252);
+            if(vm.book.description.length === 252) vm.book.description = vm.book.description + "...";
+            switch(language){
+                case "en":
+                    vm.book.language = "ENGLISCH";
+                    break;
+                case "de":
+                    vm.book.language = "DEUTSCH";
+                    break;
+                case "ru":
+                    vm.book.language = "RUSSISCH";
+                    break;
+            }
+            vm.book.cover = cover;
+            vm.book.coverContentType = "";
+            var authorName = authors.join(", ");
+            if(!vm.authors.some(function(author){
+                    if(author.name === authorName){
+                        vm.book.author = author;
+                        vm.isSaving = false;
+                        return true;
+                    }
+                    return false;
+                })){
+                Author.save({'name': authorName}, function(result){
+                    vm.authors.push(result);
+                    vm.book.author = vm.authors[vm.authors.length - 1];
+
+                    vm.isSaving = false;
+                }, function(){
+                    vm.isSaving = false;
+                });
+            }
+        }
 
         function clear () {
             $uibModalInstance.dismiss('cancel');
